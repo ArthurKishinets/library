1eb95635462bee4e465cae051627e7eeb225f4c8
Arthur Kishinets <arthurkishinets@yahoo.com.ua>
2018-07-15 10:36:14 +0300

    express

---
+++ db/dbStart.mjs
@@ -0,0 +1,30 @@
+/* import mysql from 'mysql';
+import settings from './db/dbSettings';
+import query from './query';
+import * as queries from './queries';
+
+const connection = mysql.createConnection(settings);
+
+connection.connect();
+
+const queryName = process.argv[2].split('=')[1];
+if (!queryName) {
+    console.log('You should provide query to evaluate.');
+    process.exit(1);
+} else if (!queries[queryName]) {
+    console.log('There is no such query.');
+    process.exit(1);
+}
+
+async function evalQuery() {
+    try {
+        const result = await query(connection, queries[queryName]);
+        console.log('The solution is: ', result);
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+evalQuery();
+
+connection.end(); */
--- index.mjs
+++ index.mjs
@@ -1,30 +1,5 @@
-import mysql from 'mysql';
-import settings from './dbSettings';
-import query from './query';
-import * as queries from './queries';
+import express from 'express';
 
-const connection = mysql.createConnection(settings);
+const app = express();
 
-connection.connect();
-
-const queryName = process.argv[2].split('=')[1];
-if (!queryName) {
-    console.log('You should provide query to evaluate.');
-    process.exit(1);
-} else if (!queries[queryName]) {
-    console.log('There is no such query.');
-    process.exit(1);
-}
-
-async function evalQuery() {
-    try {
-        const result = await query(connection, queries[queryName]);
-        console.log('The solution is: ', result);
-    } catch (e) {
-        console.error(e);
-    }
-}
-
-evalQuery();
-
-connection.end();
+app.listen({ port: 3000 }, () => console.log('server ready at 3000'));
