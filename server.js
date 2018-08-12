ef4bb5308690540cf845b25e44c965b52fd7d9f1
Arthur Kishinets <arthurkishinets@yahoo.com.ua>
2018-08-12 18:25:48 +0300

    Mutator

--- server.js
+++ server.js
@@ -11,13 +11,24 @@ const schema = buildSchema(`
     select_all_actors(start: Int, count: Int): [Customer!]!
   },
   type Customer {
-    cust_id: Int!,
+    cust_id: Int,
+    fed_id: String,
+    cust_type_cd: String,
+    address: String,
+    city: String,
+    state: String,
+    postal_code: String
+  }
+  input CustomerInput {
     fed_id: String!,
     cust_type_cd: String!,
-    adress: String,
+    address: String,
     city: String!,
     state: String!,
     postal_code: String!
+  }
+  type Mutation {
+      createCustomer(customer: CustomerInput): Customer
   }
 `);
 
@@ -25,6 +36,17 @@ const rootValue = {
     select_all_actors: ({ start = 0, count = 10 }) => {
         const queryString = `SELECT * FROM customer LIMIT ${mysql.escape(start)}, ${mysql.escape(count)}`;
         return query(queryString);
+    },
+    async createCustomer({
+        customer: {
+            fed_id, cust_type_cd, address, city, state, postal_code
+        }
+    }) {
+        const queryString = `INSERT INTO customer (fed_id, cust_type_cd, address, city, state, postal_code)
+            VALUES (${mysql.escape(fed_id)}, ${mysql.escape(cust_type_cd)}, ${mysql.escape(address)}, ${mysql.escape(city)}, ${mysql.escape(state)}, ${mysql.escape(postal_code)})`;
+        await query(queryString);
+        const resp = await query('SELECT * FROM customer WHERE cust_id= LAST_INSERT_ID()');
+        return resp[0];
     }
 };
 
